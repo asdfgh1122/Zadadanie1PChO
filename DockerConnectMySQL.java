ae010372ff48453cd7d993bfbf607095a6521b35
asdfgh1122 <45489243+asdfgh1122@users.noreply.github.com>
2018-11-30 15:20:48 +0100

    Update DockerConnectMySQL.java

--- DockerConnectMySQL.java
+++ DockerConnectMySQL.java
@@ -18,12 +18,7 @@ public class DockerConnectMySQL {
 
       stmt = conn.createStatement();
       String sql;
-      sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50)
-)";
+      sql = "CREATE TABLE MyGuests (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,firstname VARCHAR(30) NOT NULL,lastname VARCHAR(30) NOT NULL,email VARCHAR(50))";
 
 ResultSet rs = stmt.executeQuery(sql);
 
