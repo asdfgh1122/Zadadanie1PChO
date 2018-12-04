a3e826f628ab8b74f88c26e5a7fecce066582f71
asdfgh1122 <45489243+asdfgh1122@users.noreply.github.com>
2018-12-04 03:19:36 +0100

    Fixed ip to container with mysql.

--- DockerMySQLTest.java
+++ DockerMySQLTest.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class DockerMySQLTest {
 
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/zadanie";
+    static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/zadanie";
 
     static final String USER = "mnowosad";
     static final String PASS = "password";
